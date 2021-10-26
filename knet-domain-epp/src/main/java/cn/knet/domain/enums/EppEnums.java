e87c4adae3293d8ea9a0f5626d1f701a12e82286
xuxiannian <xuxiannian@knet.cn>
2021-10-26 16:00:14 +0800

    修改

--- knet-domain-epp/src/main/java/cn/knet/domain/enums/EppEnums.java
+++
@@ -1,7 +0,0 @@
-package cn.knet.domain.enums;
-
-public enum EppEnums {
-
-    clientDeleteProhibited, clientHold, clientRenewProhibited, clientTransferProhibited, clientUpdateProhibited, inactive, ok, pendingCreate, pendingDelete, pendingRenew, pendingTransfer, pendingUpdate, serverDeleteProhibited, serverHold, serverRenewProhibited, serverTransferProhibited, serverUpdateProhibited;
-
-}
